7b384fa3da0801108a45ad3ea3df3f453f4554ad
Oeyshik Das <oeyshikdas1853@gmail.com>
2023-10-05 16:32:37 +0530

    First Commit

--- javascript/main.js
+++ javascript/main.js
@@ -2,49 +2,51 @@
 
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY > 0)
+    ('window-scroll', window.scrollY > 0);
 })
 
 
-// show/hide faq answer
-
+// Show/hide FAQ answer
 const faqs = document.querySelectorAll('.faq');
 
 faqs.forEach(faq => {
+    const icon = faq.querySelector('.faq__icon i');
+    const answer = faq.querySelector('.question__answer p');
+    
     faq.addEventListener('click', () => {
-        faq.classList.toggle('open');
-
-        // change icon
-        const icon = faq.querySelector('.faq__icon i');
-        if(icon.className === 'uil uil-plus') {
-            icon.className = 'uil uil-minus'
+        // Toggle the 'active' class on the FAQ to show/hide the answer
+        faq.classList.toggle('active');
+        
+        // Toggle the icon class to change the icon
+        if (faq.classList.contains('active')) {
+            icon.className = 'uil uil-minus';
         } else {
             icon.className = 'uil uil-plus';
         }
-        
-    })
-})
+    });
+});
 
 
 
-// show/hide nav menu
+
+
+
+// Show/hide nav menu
 const menu = document.querySelector(".nav__menu");
 const menuBtn = document.querySelector("#open-menu-btn");
 const closeBtn = document.querySelector("#close-menu-btn");
 
-
 menuBtn.addEventListener('click', () => {
-    menu.style.display = "flex";
+    menu.classList.add("menu-open"); // Add a class to show the menu
     closeBtn.style.display = "inline-block";
     menuBtn.style.display = "none";
-})
-
+});
 
 // close nav menu
 const closeNav = () => {
-    menu.style.display = "none";
+    menu.classList.remove("menu-open"); // Remove the class to hide the menu
     closeBtn.style.display = "none";
     menuBtn.style.display = "inline-block";
-} 
+}
 
-closeBtn.addEventListener('click', closeNav)
+closeBtn.addEventListener('click', closeNav);
--- javascript/register.js
+++ javascript/register.js
@@ -1,5 +1,5 @@
-// Form Validation And Storing Form Data
- button.onclick = function() {
+// Function to validate the form fields
+function validateForm() {
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
     const dob = document.getElementById("dob").value;
@@ -8,58 +8,41 @@
     const department = document.getElementById("department").value;
     const course = document.getElementById("course").value;
     const collegeId = document.getElementById("collegeId").value;
-
-    if (firstName.trim()=="") {
-        alert("First Name cannot be empty");
-        return false;
+  
+    if (firstName.trim() === "") {
+      alert("First Name cannot be empty");
+      return false;
     }
-
-    else if (lastName.trim()=="") {
-        alert("Last Name cannot be empty");
-        return false;
+  
+    if (lastName.trim() === "") {
+      alert("Last Name cannot be empty");
+      return false;
     }
-    
-    else if (phoneNumber.length!=10) {
-        alert("Phone Number must contain 10 digits");
-        return false;
+  
+    if (phoneNumber.length !== 10) {
+      alert("Phone Number must contain 10 digits");
+      return false;
     }
-
-    else if (department.length!=4) {
-        alert("Enter Valid Department ID");
-        return false;
+  
+    if (department.length !== 4) {
+      alert("Enter Valid Department ID");
+      return false;
     }
-
-    else if (collegeId.length!=6) {
-        alert("Enter Valid College ID");
-        return false;
+  
+    if (collegeId.length !== 6) {
+      alert("Enter Valid College ID");
+      return false;
     }
-    
-    else {
-        true;
+  
+    return true;
+  }
+  
+  // Add an event listener to the form submission
+  const form = document.getElementById("registrationForm");
+  form.addEventListener("submit", function (event) {
+    if (!validateForm()) {
+      // Prevent the form from submitting if validation fails
+      event.preventDefault();
     }
-
-    let member__records = new Array();
-    member__records=JSON.parse(localStorage.getItem("Members"))?JSON.parse(localStorage.getItem("Members")):[];
-    if(member__records.some((v)=>{return v.emailId==emailId}))
-    {
-        alert("This Email Address Is Already In Use");
-        return false;
-    }
-    else
-    {
-        member__records.push({
-        "firstName":firstName,
-        "lastName":lastName,
-        "dob":dob,
-        "emailId":emailId,
-        "phoneNumber":phoneNumber,
-        "department":department,
-        "course":course,
-        "collegeId":collegeId
-    })
-
-    localStorage.setItem("Members",JSON.stringify(member__records));
-    }
-
-}
-console.log(localStorage);
+  });
+  
